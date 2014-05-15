760de834cae9207ffcaf8d9518e8715eda670e15
hakatashi <hakatasiloving@gmail.com>
2014-05-16 06:34:40 +0900

    Enable allocating

--- soroban.js
+++ soroban.js
@@ -15,6 +15,10 @@
     TAMA: {
         HEIGHT: 40,
         WIDTH: 84
+    },
+    SHADOW: {
+        HEIGHT: 80,
+        WIDTH: 116
     }
 };
 
@@ -22,16 +26,7 @@ CONST.FIELD.WIDTH = CONST.FIELD.KETA * CONST.FIELD.KETAWIDTH;
 CONST.FIELD.HEIGHT = CONST.FIELD.TOPHEIGHT + CONST.FIELD.MIDDLEHEIGHT + CONST.FIELD.BOTTOMHEIGHT;
 
 var Soroban = function (element) {
-    this.onResize = function () {
-        this.width = element.width('80%').width();
-        this.height = element.height(this.width / CONST.SOROBAN.WIDTH * CONST.SOROBAN.HEIGHT + 'px').height();
-        this.zoom = this.width / CONST.SOROBAN.WIDTH;
-
-        element.css({ 'margin-top': $(window).height() / 2 - this.height / 2 });
-    };
-
-    this.onResize();
-    $(window).resize(this.onResize);
+    var soroban = this;
 
     this.tamas = new Array();
     for (var i = 0; i < CONST.FIELD.KETA; i++) {
@@ -45,10 +40,24 @@ var Soroban = function (element) {
     this.arrange = function () {
         for (var i = 0; i < CONST.FIELD.KETA; i++) {
             for (var j = 0; j < 5; j++) {
-                this.tamas[i][j].allocate(this.zoom);
+                soroban.tamas[i][j].allocate(this.zoom);
             }
         }
     };
+    this.arrange();
+
+    this.onResize = function () {
+        soroban.width = element.width('80%').width();
+        soroban.height = element.height(soroban.width / CONST.SOROBAN.WIDTH * CONST.SOROBAN.HEIGHT + 'px').height();
+        soroban.zoom = soroban.width / CONST.SOROBAN.WIDTH;
+
+        element.css({ 'margin-top': $(window).height() / 2 - soroban.height / 2 });
+
+        soroban.arrange();
+    };
+
+    this.onResize();
+    $(window).resize(this.onResize);
 };
 
 var Tama = function (keta, number) {
@@ -73,31 +82,46 @@ var Tama = function (keta, number) {
     this.state = false;
 
     this.allocate = function (zoom) {
+        $tama.css({
+            top: (this.virtualY() - CONST.TAMA.HEIGHT / 2) * zoom + 'px',
+            left: (this.virtualX() - CONST.TAMA.WIDTH / 2) * zoom + 'px',
+            width: CONST.TAMA.WIDTH * zoom,
+            height: CONST.TAMA.HEIGHT * zoom
+        });
+        $shadow.css({
+            top: (this.virtualY() - CONST.SHADOW.HEIGHT / 2 + 5) * zoom + 'px',
+            left: (
+                    (this.virtualX() - CONST.SOROBAN.WIDTH / 2) * 0.98
+                    + CONST.SOROBAN.WIDTH / 2
+                    - CONST.SHADOW.WIDTH / 2
+                ) * zoom + 'px',
+            width: CONST.SHADOW.WIDTH * zoom,
+            height: CONST.SHADOW.HEIGHT * zoom
+        });
     }
 
     this.virtualX = function () {
         return CONST.FIELD.X
-            + CONST.FIELD.KETAWIDTH * (CONST.FIELD.KETA - this.keta - 0.5)
-            - CONST.TAMA.WIDTH / 2;
+            + CONST.FIELD.KETAWIDTH * (CONST.FIELD.KETA - this.keta - 0.5);
     }
 
     this.virtualY = function () {
         if (this.number === 0) {
             if (this.state) {
-                return CONST.FIELD.Y + CONST.FIELD.TOPHEIGHT - CONST.TAMA.HEIGHT;
+                return CONST.FIELD.Y + CONST.FIELD.TOPHEIGHT - CONST.TAMA.HEIGHT / 2;
             } else {
-                return CONST.FIELD.Y;
+                return CONST.FIELD.Y + CONST.TAMA.HEIGHT / 2;
             }
         } else {
             if (this.state) {
                 return CONST.FIELD.Y
                     + CONST.FIELD.TOPHEIGHT
                     + CONST.FIELD.MIDDLEHEIGHT
-                    + CONST.TAMA.HEIGHT * (this.number - 1);
+                    + CONST.TAMA.HEIGHT * (this.number - 0.5);
             } else {
                 return CONST.FIELD.Y
                     + CONST.FIELD.HEIGHT
-                    - CONST.TAMA.HEIGHT * (5 - this.number);
+                    - CONST.TAMA.HEIGHT * (4.5 - this.number);
             }
         }
     }
