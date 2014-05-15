4323210d14b65fe4b1ffef1e462f5b81662cfef0
hakatashi <hakatasiloving@gmail.com>
2014-05-16 00:25:32 +0900

    Kick off

---
+++ soroban.js
@@ -0,0 +1,18 @@
+﻿sorobanHeight = 318;
+sorobanWidth = 1316;
+
+var Soroban = function (element) {
+    this.onResize = function () {
+        this.width = element.width('80%').width();
+        this.height = element.height(this.width / sorobanWidth * sorobanHeight + 'px').height();
+
+        element.css({ 'margin-top': $(window).height() / 2 - this.height / 2 });
+    };
+
+    this.onResize();
+    $(window).resize(this.onResize);
+};
+
+$(document).ready(function () {
+    soroban = new Soroban($('#soroban'));
+});
